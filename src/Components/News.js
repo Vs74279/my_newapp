7b1b9a9ad82c2b7b931ee43d90df001974f22dbe
vikash kumar singh <drv74279@gmail.com>
2024-03-01 13:36:13 +0530

    Added some feature of VikashNews

--- src/Components/News.js
+++ src/Components/News.js
@@ -4,13 +4,20 @@ import NewsItem from './NewsItem'
 export default class News extends Component {
   render() {
     return (
-      <div>
-        this is a news components
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
+      <div className='container my-3'>
+          <h1>VikashNews - Top Headline</h1>
+        <div className='row'>
+        <div className='col-md-4'>
+            <NewsItem title="MyTitle" description="Mydesc"/>
+        </div>
+        <div className='col-md-4'>
+            <NewsItem title="MyTitle" description="Mydesc"/>
+        </div>
+        <div className='col-md-4'>
+            <NewsItem title="MyTitle" description="Mydesc"/>
+        </div>
+        
+        </div>
         
       </div>
     )
